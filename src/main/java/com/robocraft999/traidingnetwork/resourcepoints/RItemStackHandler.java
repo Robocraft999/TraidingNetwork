053dc28b4c81d860d885b9f386f56e289e3505bb
Robocraft999 <Alexakeil987@gmail.com>
2024-03-04 15:25:33 +0100

    fixed itemstacks properly searializing due to amount being saved as byte

--- src/main/java/com/robocraft999/traidingnetwork/resourcepoints/RItemStackHandler.java
+++ src/main/java/com/robocraft999/traidingnetwork/resourcepoints/RItemStackHandler.java
@@ -3,6 +3,7 @@ package com.robocraft999.traidingnetwork.resourcepoints;
 import com.robocraft999.traidingnetwork.TraidingNetwork;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
@@ -32,6 +33,7 @@ public class RItemStackHandler extends ItemStackHandler {
     }
 
     public NonNullList<ItemStack> getItems() {
+        //TraidingNetwork.LOGGER.info("des get: "+stacks);
         return this.stacks;
     }
 
@@ -59,6 +61,46 @@ public class RItemStackHandler extends ItemStackHandler {
 
     public boolean hasFreeSlot(ItemStack stackInSlot) {
         return stacks.stream().anyMatch((stack) -> stack.isEmpty() || stack.is(stackInSlot.getItem()));
+    }
+
+    @Override
+    public CompoundTag serializeNBT() {
+        ListTag nbtTagList = new ListTag();
+
+        for(int i = 0; i < this.stacks.size(); ++i) {
+            if (!((ItemStack)this.stacks.get(i)).isEmpty()) {
+                CompoundTag itemTag = new CompoundTag();
+                itemTag.putInt("Slot", i);
+                itemTag.putInt("RealCount", this.stacks.get(i).getCount());
+                ((ItemStack)this.stacks.get(i)).save(itemTag);
+                nbtTagList.add(itemTag);
+            }
+        }
+
+        CompoundTag nbt = new CompoundTag();
+        nbt.put("Items", nbtTagList);
+        nbt.putInt("Size", this.stacks.size());
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag nbt) {
+        this.setSize(nbt.contains("Size", 3) ? nbt.getInt("Size") : this.stacks.size());
+        ListTag tagList = nbt.getList("Items", 10);
+
+        for(int i = 0; i < tagList.size(); ++i) {
+            CompoundTag itemTags = tagList.getCompound(i);
+            int slot = itemTags.getInt("Slot");
+            if (slot >= 0 && slot < this.stacks.size()) {
+                ItemStack desItemStack = ItemStack.of(itemTags);
+                desItemStack.setCount(itemTags.getInt("RealCount"));
+                TraidingNetwork.LOGGER.info("stack: "+desItemStack);
+                this.stacks.set(slot, desItemStack);
+            }
+        }
+        TraidingNetwork.LOGGER.info("des stacks: "+stacks);
+
+        this.onLoad();
     }
 
     public void enlarge(){
